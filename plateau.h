1f48a245ae646585c5aac2a793e1d08b8acaf526
jeangardyf20 <jeangardyf20@gmail.com>
2026-08-14 13:54:36 -0500

    Projet Laska

--- plateau.h
+++ plateau.h
@@ -1,2 +1,8 @@
-void soldat( SDL_Renderer* pinceau, int x , int y,int cR,int cB);
+void soldat( SDL_Renderer* pinceau, int x , int y,int cR,int cB,int nc);
 void ligne(SDL_Renderer* pinceau);
+void selection(SDL_Renderer*pinceau, SDL_Event event);
+void Accueil(SDL_Renderer* pinceau);
+void pos_case(int i, int j, int*x,int*y);
+int v_deplacement(int x1,int y1,int x2,int y2,int joueur);
+typedef struct
+{int joueur;int officier;int prise}pion;
